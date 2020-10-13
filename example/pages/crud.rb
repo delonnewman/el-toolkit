205f4e5a44348b88d2c192be2ac226f462cd25d4
Delon Newman <delnewman@salud.unm.edu>
2020-10-12 21:13:29 -0600

    Adding create operation, it's adding but not refreshing

--- example/pages/crud.rb
+++ example/pages/crud.rb
@@ -16,28 +16,52 @@ module Examples
       def render html
         app.view(:navbar) +
           html.div(class: 'container') {
-            html.table(class: 'table') {
-              [
-                html.thead {
-                  [ html.th(content: 'Name'),
-                    html.th(content: 'Age') ]
-                },
-                html.tbody {
-                  DB[:people].map { |person|
-                    html.tr {
-                      [ html.td(content: person[:name]),
-                        html.td(content: person[:age]) ]
-                    }
-                  }
-                }
-              ]
-            }
+            table(html) + form(html)
           }
       end
 
       private
 
+      def table(html)
+        html.table(class: 'table') do
+          [
+            html.thead do
+              [ html.th(content: 'Name'),
+                html.th(content: 'Age') ]
+            end,
+            html.tbody do
+              DB[:people].map do |person|
+                row(html, person)
+              end
+            end
+          ]
+        end
+      end
 
-    end
-  end
-end
+      def form(html)
+        html.form do
+          [ html.input(id: 'crud-name', type: 'text'),
+            html.input(id: 'crud-age', type: 'text'),
+            html.button(type: 'button', on: { click: add_row(html) }, content: 'Add')
+          ]
+        end
+      end
+
+      def add_row(html)
+        lambda do
+          html.select('#crud-name').value.then(->(name) {
+            html.select('#crud-age').value.then(->(age) {
+              DB[:people].insert(name: name, age: age)
+            })
+          })
+        end
+      end
+
+      def row(html, person)
+        html.tr do
+          html.td(content: person[:name]) + html.td(content: person[:age])
+        end
+      end
+    end # crud
+  end # pages
+end # examples
