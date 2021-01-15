d88719df1731d6f7a6a375aff3d473e1507bf1fe
Delon Newman <contact@delonnewman.name>
2021-01-14 17:01:56 -0700

    Add El::Markup::Document

---
+++ lib/el/markup/document.rb
@@ -0,0 +1,15 @@
+module El
+  class Markup
+    class Document
+      def initialize
+        @by_id_index    = {}
+        @by_tag_index   = {}
+        @by_class_index = {}
+      end
+
+      def add_element(element)
+        
+      end
+    end
+  end
+end
