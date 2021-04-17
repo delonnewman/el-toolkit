8b85c4b088eaeb7f5aebd5c507f08906e095cdd3
Delon Newman <contact@delonnewman.name>
2021-04-17 17:17:56 -0600

    specs are passing

--- spec/el/markup_spec.rb
+++ spec/el/markup_spec.rb
@@ -1,53 +1,53 @@
-module El
-  class MarkupTest < Minitest::Test
-    def setup
-      @html = Document[:HTML5]
-      @xhtml = Document[:XHTML]
+RSpec.describe El::Document do
+  let(:html) { described_class[:HTML] }
+  let(:xhtml) { described_class[:XHTML] }
 
-      @examples = [
-        { test: ->(markup) { markup.br.to_markup },
-          html: '<br>',
-          xhtml: '<br/>' },
-        { test: ->(markup) { markup.link(rel: 'stylesheet', href: '/styles/app.css').to_markup },
-          html: "<link rel='stylesheet' href='/styles/app.css'>",
-          xhtml: '<link rel="stylesheet" href="/styles/app.css"/>' },
-        { test: ->(markup) { markup.a(href: '#', required: true, class: %w[btn btn-primary]).to_markup },
-          html: "<a href='#' required class='btn btn-primary'></a>",
-          xhtml: '<a href="#" required="required" class="btn btn-primary"></a>' }
-      ]
+  it "should generate code according to it's doctype" do
+    examples = [
+      { test: ->(markup) { markup.br.to_markup },
+        html: '<br>',
+        xhtml: '<br/>' },
+      { test: ->(markup) { markup.link(rel: 'stylesheet', href: '/styles/app.css').to_markup },
+        html: "<link rel='stylesheet' href='/styles/app.css'>",
+        xhtml: '<link rel="stylesheet" href="/styles/app.css"/>' },
+      { test: ->(markup) { markup.a(href: '#', required: true, class: %w[btn btn-primary]).to_markup },
+        html: "<a href='#' required class='btn btn-primary'></a>",
+        xhtml: '<a href="#" required="required" class="btn btn-primary"></a>' }
+    ]
+
+    examples.each do |example|
+      expect(example[:test].call(html)).to eq example[:html]
+      expect(example[:test].call(xhtml)).to eq example[:xhtml]
     end
+  end
 
-    def test_is_xml
-      assert !@html.xml?, "html is not xml"
-      assert @xhtml.xml?, "xhtml is xml"
+  context '#xml?' do
+    it 'should return true if the doctype is xml, false otherwise' do
+      expect(html.xml?).to be false
+      expect(xhtml.xml?).to be true
 
-      assert !@html.br.xml?, "html elements are not xml"
-      assert @xhtml.br.xml?, "xhtml elements are xml"
-
-      assert_equal '<br>', @html.br.to_markup
-      assert_equal '<br/>', @xhtml.br.to_markup
+      expect(html.br.xml?).to be false
+      expect(xhtml.br.xml?).to be true
     end
+  end
 
-    def test_examples
-      @examples.each do |example|
-        assert_equal example[:html], example[:test].call(@html), example[:html]
-        assert_equal example[:xhtml], example[:test].call(@xhtml), example[:xhtml]
-      end
-    end
-
-    def test_markup_block
-      code1 = El::Document[:HTML] do
+  context 'document block' do
+    it 'should generate markup implicitly' do
+      code = El::Document[:HTML] do
         a(href: '#') { 'Testing' } + br
         br
       end.to_s
 
-      code2 = El::Document[:HTML] do |html|
+      expect(code.lines.map(&:chomp).join('')).to eq "<a href='#'>Testing</a><br><br>"
+    end
+
+    it 'should generate markup explicitly' do
+      code = El::Document[:HTML] do |html|
         html.a(href: '#') { 'Testing' } + html.br
         html.br
       end.to_s
 
-      assert_equal "<a href='#'>Testing</a><br><br>", code1.lines.map(&:chomp).join('')
-      assert_equal "<a href='#'>Testing</a><br><br>", code2.lines.map(&:chomp).join('')
+      expect(code.lines.map(&:chomp).join('')).to eq "<a href='#'>Testing</a><br><br>"
     end
   end
 end
