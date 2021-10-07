577ac78ad6dd29c8e80f2772939a1b4a48173ac4
Delon Newman <contact@delonnewman.name>
2021-10-07 12:11:07 -0600

    Add to_s and inspect to El::Null spec

--- el-core/spec/el/null_spec.rb
+++ el-core/spec/el/null_spec.rb
@@ -1,7 +1,19 @@
 require 'el/null'
 
 RSpec.describe El::Null do
-  it "returns itself with any method call" do
+  it "returns itself with any method call except to_s and inspect" do
     expect(El::Null.anything).to be El::Null
+  end
+
+  describe 'to_s' do
+    it "returns 'El::Null'" do
+      expect(El::Null.to_s).to eq 'El::Null'
+    end
+  end
+
+  describe 'inspect' do
+    it "returns 'El::Null'" do
+      expect(El::Null.inspect).to eq 'El::Null'
+    end
   end
 end
