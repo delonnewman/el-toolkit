1754d41ca8807ef1f0845db3a11575063c57d155
Delon Newman <contact@delonnewman.name>
2022-02-07 23:11:27 -0700

    Add metadata macros to El::Base

--- el-core/lib/el/base.rb
+++ el-core/lib/el/base.rb
@@ -16,6 +16,40 @@ module El
           instance_variable_get("@{name}")
         end
       end
+
+      def class_doc(doc)
+        class_meta(doc: doc)
+      end
+
+      def class_meta(data)
+        @class_metadata = data
+      end
+
+      def class_metadata
+        @class_metadata
+      end
+
+      def doc(doc)
+        meta(doc: doc)
+      end
+    
+      def meta(data)
+        @_metadata = data
+      end
+    
+      def add_method_metadata(method, data)
+        @method_metadata ||= {}
+        @method_metadata[method] = data
+      end
+    
+      def method_metadata(method)
+        @method_metadata[method]
+      end
+      
+      def method_added(method)
+        add_method_metadata(method, @_metadata)
+        @_metadata = nil
+      end
     end
   end
 end
