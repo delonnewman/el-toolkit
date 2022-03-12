2291601aa821c3e0cc288538a2842eba8ee4b080
Delon R. Newman <contact@delonnewman.name>
2022-03-11 21:00:23 -0700

    Make El::Rouable::Request#request_method return raw method

--- el-routing/lib/el/routable/request.rb
+++ el-routing/lib/el/routable/request.rb
@@ -14,6 +14,22 @@ module El
         @route_params = route_params
       end
 
+      def []=(_, _)
+        raise 'Mutating parameters is not supported'
+      end
+
+      def delete_param(_)
+        raise 'Mutating parameters is not supported'
+      end
+
+      def update_param(_, _)
+        raise 'Mutating parameters is not supported'
+      end
+
+      def path_info=(_)
+        raise 'Mutating parameters is not supported'
+      end
+
       def to_h
         @env.dup
       end
@@ -34,11 +50,23 @@ module El
       alias each each_header
 
       def request_method
-        @env['REQUEST_METHOD'].downcase.to_sym
+        @env['REQUEST_METHOD']
       end
 
       def session
         @env['rack.session']
+      end
+
+      def hijack?
+        @env['rack.hijack?']
+      end
+
+      def hijack
+        @env['rack.hijack']
+      end
+
+      def hijack_io
+        @env['rack.hijack_io']
       end
 
       def path
@@ -95,11 +123,16 @@ module El
         Rack::MediaType.type(content_type)
       end
 
-      JSON_MEDIA_TYPE = 'application/json'
+      JSON_MEDIA_TYPES = Set[
+        'application/json',
+        'application/vnd.api+json',
+        'application/ld+json',
+        'text/json'
+      ].freeze
 
-      def json_body(symbolize_names: true)
+      def json_body(symbolize_names: true, force: false)
         return @json_body if @json_body
-        return EMPTY_HASH unless request_method == 'POST' && media_type == JSON_MEDIA_TYPE
+        return EMPTY_HASH unless force || request_method == 'POST' && JSON_MEDIA_TYPES.include?(media_type)
 
         @json_body = JSON.parse(body.read, symbolize_names: symbolize_names).tap do
           body.rewind
@@ -108,7 +141,7 @@ module El
       alias json json_body
 
       def params
-        @params ||= route_params.merge(body_params, query_params)
+        @params ||= route_params.merge(body_params, query_params, json_body)
       end
 
       def query_params
@@ -118,14 +151,14 @@ module El
       # The set of form-data media-types. Requests that do not indicate
       # one of the media types present in this list will not be eligible
       # for form-data / param parsing.
-      FORM_DATA_MEDIA_TYPES = [
+      FORM_DATA_MEDIA_TYPES = Set[
         'application/x-www-form-urlencoded',
         'multipart/form-data'
       ].freeze
 
       def body_params
         return @body_params if @body_params
-        return EMPTY_HASH unless @env['REQUEST_METHOD'] == 'POST' && FORM_DATA_MEDIA_TYPES.include?(media_type)
+        return EMPTY_HASH unless request_method == 'POST' && FORM_DATA_MEDIA_TYPES.include?(media_type)
 
         body.tap do |body|
           @body_params = DataUtils.parse_form_encoded_data(body.read)
